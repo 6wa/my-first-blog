a609e45f58935284a10edd7129134ef72c06ebcd
Altynai <altynai.ngbv@gmail.com>
2019-04-01 00:34:44 +0300

    My first app commit

---
+++ python_test.py
@@ -0,0 +1,55 @@
+# name = "alty"
+#
+# if (name == 'alty'):
+#     print('hello '+name)
+#
+# elif (name == 'pasha'):
+#     print ('hello'+name)
+#
+# else:
+#     print ('unknown')
+#
+# volume = 23
+# if volume < 20:
+#     print("It's kinda quiet.")
+# elif 20 <= volume < 40:
+#     print("It's nice for background music")
+# elif 40 <= volume < 60:
+#     print("Perfect, I can hear all the details")
+# elif 60 <= volume < 80:
+#     print("Nice for parties")
+# elif 80 <= volume < 100:
+#     print("A bit loud!")
+# else:
+#     print("My ears are hurting! :(")
+
+
+# def hi():
+#     print('Hi there!')
+#     print('How are you?')
+#
+# hi()
+
+
+# def sayHi(name):
+#     if (name == 'alty'):
+#         print('hello alty!')
+#
+#     elif (name == 'pasha'):
+#         print ('hello pasha')
+#
+#     else:
+#         print ('unknown')
+#
+# sayHi('so')
+#
+# def greeting (name):
+#     print ('hello '+name+'!')
+#
+# girls = ['lera', 'dasha','anna', 'a;ty', 'galina', 'kamilya']
+#
+# for name in girls :
+#     greeting(name)
+
+for i in range(1, 10, 2):
+    print(i)
